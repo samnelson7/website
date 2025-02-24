8fc96d9d937c94863b952add14f705372f8d5876
Sam Nelson <samnelson7@hotmail.com>
2025-02-23 18:09:14 -0800

    Split Projects text into smaller paragraphs to make it easier to read

--- sams-website/src/app/projects/page.tsx
+++ sams-website/src/app/projects/page.tsx
@@ -13,7 +13,7 @@ import {
 
 interface Project {
   title: string;
-  description: string;
+  description: string[];
   mediaUrl: string;
   mediaType: "image" | "video";
   technologies: string[];
@@ -23,7 +23,7 @@ interface Project {
 const projects: Project[] = [
   {
     title: "Weld Access Verification Tool",
-    description: "This was a project I completed through my most recent co-op term. I don't generally include work projects but developing prototypes was largely self guided and completed with my fellow co-op student as a pair so I wanted to showcase it here. The AGT Robotics BeamMaster WAV (Weld Access Verification) Tool is an innovative Tekla plugin that revolutionizes weld planning in steel fabrication. By automatically integrating true-to-scale weld arm models into your Tekla environment, this powerful tool allows designers and engineers to visualize real-world welding scenarios, instantly identify potential access issues, and optimize projects for AGT BeamMaster robotic welding systems. I worked on this application during my time at Exact Detailing with one other co-op student. We found ourselves incredibly dedicated to the completion of this project and were able to complete and publish it over the course of our 4-month internships. We picked up the project from where some previous students had left off but wound up reworking most of the application due to changing business goals and project requirements. Working closely with AGT Robotics, we were able to publish this to an online catalog, Tekla Warehouse, where it can be downloaded for use by anybody with a Tekla license.",
+    description: ["This was a project I completed through my most recent co-op term. I don't generally include work projects but developing prototypes was largely self guided and completed with my fellow co-op student as a pair so I wanted to showcase it here.", "The AGT Robotics BeamMaster WAV (Weld Access Verification) Tool is an innovative Tekla plugin that revolutionizes weld planning in steel fabrication. By automatically integrating true-to-scale weld arm models into your Tekla environment, this powerful tool allows designers and engineers to visualize real-world welding scenarios, instantly identify potential access issues, and optimize projects for AGT BeamMaster robotic welding systems.", "I worked on this application during my time at Exact Detailing with one other co-op student. We found ourselves incredibly dedicated to the completion of this project and were able to complete and publish it over the course of our 4-month internships. We picked up the project from where some previous students had left off but wound up reworking most of the application due to changing business goals and project requirements. Working closely with AGT Robotics, we were able to publish this to an online catalog, Tekla Warehouse, where it can be downloaded for use by anybody with a Tekla license."],
     mediaUrl: "/videos/WAVDemo.mp4",
     mediaType: "video",
     technologies: ["C#", ".NET", "WPF", "Windows Forms"],
@@ -31,7 +31,7 @@ const projects: Project[] = [
   },
   {
     title: "VirtualER",
-    description: "This web-based application allows patients to easily find wait times of nearby emergency rooms to meet their medical needs. The project consisted of both a full implementation of the product as well as a detailed requirements phase, diagrams, and project planning. The project was for a Software Architecture class, but our design, tools used, approach, and system was all original work from our 4 person team. I would have liked to spend more time consulting with healthcare workers in order to more closely meet the needs of the actual healthcare field rather than the project requirements that were outlined. I am confident that we met the requirements for the scope of our project, but I feel like the application would need some serious overhauls to be used in any sort of workplace. One thing that I believe is a bit of an oversight with this project is that many workplaces simply don't have the staff to support an app like this. It requires too much help from ER staff to run properly and in order to be automated there are serious ethical and legal considerations involved.",
+    description: ["This web-based application allows patients to easily find wait times of nearby emergency rooms to meet their medical needs. The project consisted of both a full implementation of the product as well as a detailed requirements phase, diagrams, and project planning. The project was for a Software Architecture class, but our design, tools used, approach, and system was all original work from our 4 person team.", "I would have liked to spend more time consulting with healthcare workers in order to more closely meet the needs of the actual healthcare field rather than the project requirements that were outlined. I am confident that we met the requirements for the scope of our project, but I feel like the application would need some serious overhauls to be used in any sort of workplace.", "One thing that I believe is a bit of an oversight with this project is that many workplaces simply don't have the staff to support an app like this. It requires too much help from ER staff to run properly and in order to be automated there are serious ethical and legal considerations involved."],
     mediaUrl: "/images/VirtualER.png",
     mediaType: "image",
     technologies: ["Next.js", "React", "Tailwind CSS", "ShadCN Component Library"],
@@ -39,7 +39,7 @@ const projects: Project[] = [
   },
   {
     title: "Automated Feedback for Engineering",
-    description: "In the Summer of 2022 an advisor within the faculty of engineering reached out to me suggesting that I apply for a research grant based on my academic standing. Together with Dr. Flavio Firmani, I applied for funding towards a joint research project in order to provide students with automated feedback for homework problems and improve learning outcomes. Dr. Firmani identified that students frequently make similar mistakes, but due to the volume of students, individualized feedback was impossible. This led to our research into both diagnosing frequent mistakes made by students and into the kinds of tools we had available to improve the learning outcomes based on patterns we found. After gaining some insight into frequent problems made by students, we looked into the tools provided within WeBWork, the online homework system used for the statics class we were investigating. The main outcomes of our research included generating graphs for students automatically based on their inputs to provide a visual aid and automated feedback messages for various pre-defined inputs. Given more time, I would've liked to collect data based on student submissions in order to better categorize common mistakes.",
+    description: ["In the Summer of 2022 an advisor within the faculty of engineering reached out to me suggesting that I apply for a research grant based on my academic standing. Together with Dr. Flavio Firmani, I applied for funding towards a joint research project in order to provide students with automated feedback for homework problems and improve learning outcomes. Dr. Firmani identified that students frequently make similar mistakes, but due to the volume of students, individualized feedback was impossible.", "This led to our research into both diagnosing frequent mistakes made by students and into the kinds of tools we had available to improve the learning outcomes based on patterns we found. After gaining some insight into frequent problems made by students, we looked into the tools provided within WeBWork, the online homework system used for the statics class we were investigating.", "The main outcomes of our research included generating graphs for students automatically based on their inputs to provide a visual aid and automated feedback messages for various pre-defined inputs. Given more time, I would've liked to collect data based on student submissions in order to better categorize common mistakes."],
     mediaUrl: "/images/VKURA.jpg",
     mediaType: "image",
     technologies: ["Perl", "WeBWork", "PG"],
@@ -47,7 +47,7 @@ const projects: Project[] = [
   },
   {
     title: "Personal Website",
-    description: "After several years of toying with the idea of coding a website myself amongst a busy school schedule I made time to practice my development skills with web-based applications. The result is this portfolio of my education, work, skills, and projects that I am especially passionate about. Following the development of VirtualER, I wanted to create a Next.js application to function as a personal website while I was still learning these new technologies. I am very happy with how it turned out and welcome any feedback! Click on my icon or visit the contact me page to reach out and let me know what you think!",
+    description: ["After several years of toying with the idea of coding a website myself amongst a busy school schedule I made time to practice my development skills with web-based applications. The result is this portfolio of my education, work, skills, and projects that I am especially passionate about.", "Following the development of VirtualER, I wanted to create a Next.js application to function as a personal website while I was still learning these new technologies. I am very happy with how it turned out and welcome any feedback! Click on my icon or visit the contact me page to reach out and let me know what you think!"],
     mediaUrl: "/images/Website.png",
     mediaType: "image",
     technologies: ["Next.js", "React", "Tailwind CSS", "MaterialUI"],
@@ -55,7 +55,7 @@ const projects: Project[] = [
   },
   {
     title: "Business Analytics Improvement Plan",
-    description: "",
+    description: [""],
     mediaUrl: "/images/RealTime.png",
     mediaType: "image",
     technologies: ["Figma", "Markdown"],
@@ -63,7 +63,7 @@ const projects: Project[] = [
   },
   {
     title: "Autonmous IR Detection Robot",
-    description: "This project involved building a robot as a prototype for an autonomous underwater vehicle able to position a debris cleaning device on top of underwater cameras and sensors. The task required of this robot was to locate a randomly placed infrared emitter in an enclosed space, approach it, and drop a ping-pong ball on top of it without it falling off. As can be seen in the image of our robot design, we have an arm that extends in front of the robot with gears that twist open to drop the ball on the target. I assisted with the electrical work, design, and overall construction of the robot, but my biggest contribution was designing a finite state machine for our workflow and implementing that in code. Our robot was one of few able to pass all tests consistently without hitting any walls or the target.",
+    description: ["This project involved building a robot as a prototype for an autonomous underwater vehicle able to position a debris cleaning device on top of underwater cameras and sensors. The task required of this robot was to locate a randomly placed infrared emitter in an enclosed space, approach it, and drop a ping-pong ball on top of it without it falling off.", "As can be seen in the image of our robot design, we have an arm that extends in front of the robot with gears that twist open to drop the ball on the target. I assisted with the electrical work, design, and overall construction of the robot, but my biggest contribution was designing a finite state machine for our workflow and implementing that in code. Our robot was one of few able to pass all tests consistently without hitting any walls or the target."],
     mediaUrl: "/images/Robot.jpg",
     mediaType: "image",
     technologies: ["RobotC", "C", "Vex Robotics Kit"],
@@ -181,19 +181,32 @@ export default function ProjectsPage() {
                       {project.title === "Business Analytics Improvement Plan" ? (
                         <>
                           This project consisted of eliciting requirements from a company for software development improvements they desired and developing a prototype based on the requirements. 
-          We worked with a company called Real Time Networks to elicit requirements and create a requirements document for the software they wanted, as well as prototyping a potential solution. 
-          Their company contained numerous departments: marketing, manufacturing, accounting, and installation, and each department managed its own data footprint independently. 
-          The software solution they wanted was a centralized data analytics dashboard to better concentrate their data footprint. This would allow management to be better informed
-          of the work and ongoings of various departments from a single place. We developed an entire prototype for a web-based platform
-          combining their data analytics and platforms as well as suggesting how to implement it with various APIs.
+                          We worked with a company called Real Time Networks to elicit requirements and create a requirements document for the software they wanted, as well as prototyping a potential solution. 
                           <br />
+                          <br />
+                          Their company contained numerous departments: marketing, manufacturing, accounting, and installation, and each department managed its own data footprint independently. 
+                          
+                          The software solution they wanted was a centralized data analytics dashboard to better concentrate their data footprint. This would allow management to be better informed
+                          of the work and ongoings of various departments from a single place. 
+                          <br />
+                          <br />
+                          We developed an entire prototype for a web-based platform
+                          combining their data analytics and platforms as well as suggesting how to implement it with various APIs.
                           You can take a look at the complete requirements document as well as some of the prototype&#39;s features at the bottom of it{" "}
                           <Link href="https://github.com/samnelson7/Requirements-Engineering/blob/main/RD.md">
                             here
                           </Link>.
                         </>
                       ) : (
-                        project.description
+                        <>
+                        {project.description[0]}
+                        <br />
+                        <br />
+                        {project.description[1]}
+                        <br />
+                        <br />
+                        {project.description[2]}
+                        </>
                       )}
                     </Typography>
 
