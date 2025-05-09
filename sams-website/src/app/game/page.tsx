ae6509410cfcfc9797ab08b52ff18afcf6cb277c
Sam Nelson <samnelson7@hotmail.com>
2025-05-08 19:40:34 -0700

    Added temp /game extension to test hosting Fractured Flight on website

---
+++ sams-website/src/app/game/page.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { CssBaseline } from '@mui/material';
+import dynamic from 'next/dynamic';
+import Header from '../../components/Header';
+import { useEffect, useRef } from 'react';
+
+// Dynamically load UnityViewer to disable SSR
+function UnityViewerComponent() {
+  const unityInstanceRef = useRef(null);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '/WebGame/Build/WebGame.loader.js';
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, []);
+
+  return (
+    <div style={{ width: '960px', height: '600px', margin: '0 auto' }}>
+      <canvas id="unity-canvas" width="960" height="600" />
+    </div>
+  );
+}
+
+// Main Page Component
+export default function UnityPage() {
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <div style={{ paddingTop: '2rem' }}>
+        <iframe src="/WebGame/index.html" width={"100%"} height={"100%"} style={{border: "none", position: "absolute"}} allowFullScreen>
+
+        </iframe>
+        <UnityViewerComponent />
+      </div>
+    </>
+  );
+}
